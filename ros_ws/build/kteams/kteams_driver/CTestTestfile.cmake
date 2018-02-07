019a32462b6d51b544530fffa9271887908d43df
Rafael <rafael@cscruz.org>
2018-02-07 14:51:51 +0100

    Cleaned the build and devel folders

--- ros_ws/build/kteams/kteams_driver/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rafael/dev/sir/BrainInterfaceFinal/ros_ws/src/kteams/kteams_driver
-# Build directory: /home/rafael/dev/sir/BrainInterfaceFinal/ros_ws/build/kteams/kteams_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(src)
--- ros_ws/build/kteams/kteams_node/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rafael/dev/sir/BrainInterfaceFinal/ros_ws/src/kteams/kteams_node/src
-# Build directory: /home/rafael/dev/sir/BrainInterfaceFinal/ros_ws/build/kteams/kteams_node/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
